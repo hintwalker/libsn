b525317d183d6855296b220421c6bad821dfe76a
Hint Walker <hintwalker@gmail.com>
2022-04-02 16:42:59 +0700

    Fixed functuon getChiOfGregorianDay, with time is added as parameter

--- libsunoom/src/main/java/com/tauari/libsunoom/usecase/gregorian/getCanOfGregorianDay.kt
+++ libsunoom/src/main/java/com/tauari/libsunoom/usecase/gregorian/getCanOfGregorianDay.kt
@@ -1,10 +1,14 @@
 package com.tauari.libsunoom.usecase.gregorian
 
 import com.tauari.libsunoom.domain.GregorianDate
+import com.tauari.libsunoom.domain.SunoomTime
 import com.tauari.libsunoom.enums.Can
 import com.tauari.libsunoom.usecase.jdn.getJdnAt12AmUT
 
-fun getCanOfGregorianDay(date: GregorianDate): Can {
-    val jdn = getJdnAt12AmUT(date.dayOfMonth, date.month, date.year)
+fun getCanOfGregorianDay(date: GregorianDate, time: SunoomTime? = null): Can {
+    var jdn = getJdnAt12AmUT(date.dayOfMonth, date.month, date.year)
+    if(time?.hour == 23) {
+        jdn += 1
+    }
     return Can.fromInt( (jdn + 9) % 10)
 }
--- libsunoom/src/main/java/com/tauari/libsunoom/usecase/gregorian/getChiOfGregorianDay.kt
+++ libsunoom/src/main/java/com/tauari/libsunoom/usecase/gregorian/getChiOfGregorianDay.kt
@@ -1,10 +1,14 @@
 package com.tauari.libsunoom.usecase.gregorian
 
 import com.tauari.libsunoom.domain.GregorianDate
+import com.tauari.libsunoom.domain.SunoomTime
 import com.tauari.libsunoom.enums.Chi
 import com.tauari.libsunoom.usecase.jdn.getJdnAt12AmUT
 
-fun getChiOfGregorianDay(date: GregorianDate): Chi {
-    val jdn = getJdnAt12AmUT(date.dayOfMonth, date.month, date.year)
+fun getChiOfGregorianDay(date: GregorianDate, time: SunoomTime?): Chi {
+    var jdn = getJdnAt12AmUT(date.dayOfMonth, date.month, date.year)
+    if(time?.hour == 23) {
+        jdn += 1
+    }
     return Chi.fromInt((jdn + 1) % 12)
 }
